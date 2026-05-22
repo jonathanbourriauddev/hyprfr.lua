b9af2bc2614b0fe5a48b756fc49eb55f389bd6ce
jonathanbourriauddev <jonathan.bourriauddev@gmail.com>
2026-05-22 10:28:30 +0200

    feat: hypridle, numlock, swayosd waybar, fastfetch grimoire

--- hypr/.config/hypr/lua/autostart.lua
+++ hypr/.config/hypr/lua/autostart.lua
@@ -5,6 +5,7 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("~/dotfiles/scripts/start-hyprpaper.sh")
 	hl.exec_cmd("waybar")
+	hl.exec_cmd("swayosd-server")
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
--- hypr/.config/hypr/lua/input.lua
+++ hypr/.config/hypr/lua/input.lua
@@ -3,23 +3,24 @@
 -- ============================================================
 
 hl.config({
-    input = {
-        kb_layout    = "fr",
-        kb_variant   = "",
-        kb_model     = "",
-        kb_options   = "",
-        kb_rules     = "",
-        follow_mouse = 1,
-        sensitivity  = 0,
-        touchpad = {
-            natural_scroll    = true,
-            disable_while_typing = true,
-        },
-    },
+	input = {
+		kb_layout = "fr",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
+		numlock_by_default = true,
+		follow_mouse = 1,
+		sensitivity = 0,
+		touchpad = {
+			natural_scroll = true,
+			disable_while_typing = true,
+		},
+	},
 })
 
 hl.gesture({
-    fingers   = 3,
-    direction = "horizontal",
-    action    = "workspace",
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
